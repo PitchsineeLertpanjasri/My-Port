989fcc72ba1b48f3a25421895cbfe430aa813e7b
LAPTOP-ISEKNL7Q\Asus <janny20.1.2549@gmail.com>
2025-01-03 10:06:52 +0700

    reak

--- src/app/achievements/page.tsx
+++ src/app/achievements/page.tsx
@@ -4,9 +4,9 @@ export default function AchievementsPage() {
       <section id="achievements" className="my-8 bg-gray-50 p-8 rounded shadow-md">
         <h2 className="text-2xl font-bold mb-4">Achievements / Certificates</h2>
         <ul className="list-disc pl-6">
-          <li>Best Innovative Project 2022</li>
+          {/* <li>Best Innovative Project 2022</li>
           <li>Full-stack Developer Certification</li>
-          <li>Data Science Competition Winner</li>
+          <li>Data Science Competition Winner</li> */}
         </ul>
       </section>
     </div>
