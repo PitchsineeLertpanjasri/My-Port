58a48e3a80b8d2bd5949d21aafe5752380c95134
LAPTOP-ISEKNL7Q\Asus <janny20.1.2549@gmail.com>
2025-01-04 23:19:55 +0700

    tel

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -5,7 +5,7 @@ export default function Contact() {
         <ul className="text-lg">
           <li>Email: <a href="mailto:jxvv01.1245@gmail.com" className="text-white hover:text-blue-800 underline">janny20.1.2549@gmail.com</a></li>
           <li>Alt Email: <a href="mailto:6533277521@student.chula.ac.th" className="text-white hover:text-blue-800 underline">6633167421@student.chula.ac.th</a></li>
-          <li>Phone: 083-494-1944</li>
+          <li>Phone: 083-414-9494</li>
           <li><a href="https://github.com/pitchsineelertpanjasri" target="_blank" className="text-white hover:text-blue-800 underline">GitHub</a></li>
         </ul>
       </section>
